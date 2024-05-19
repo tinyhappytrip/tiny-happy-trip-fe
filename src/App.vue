47856db9385be5b0c9d23a0d213e16605dee2c64
mrcsbin <mrcsbin@naver.com>
2024-05-19 15:15:08 +0900

    fix: 회원가입 라우트수정

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,37 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { computed, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import { setCookie } from './util/cookie'
 
+const router = useRouter()
+const authStore = useAuthStore()
 const route = useRoute()
+
+const handleAuthFromUrl = () => {
+  const params = new URLSearchParams(window.location.search)
+  const paramsObj = Object.fromEntries(params.entries())
+  const { accessToken, refreshToken } = paramsObj
+  if (accessToken) {
+    const data = {
+      accessToken,
+      refreshToken
+    }
+    setCookie(data)
+    authStore.checkAuth()
+
+    router.replace('/')
+  }
+}
+
 const layout = computed(() => {
   return route.meta.layout || 'div'
+})
+
+onMounted(() => {
+  handleAuthFromUrl()
 })
 </script>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,27 @@
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 export const routes = [
-  { path: '/', name: 'main', component: () => import('@/views/MainView.vue'), meta: { layout: DefaultLayout } },
+  { path: '/', name: 'main', component: () => import('@/views/MainView.vue') },
   { path: '/login', name: 'login', component: () => import('@/views/LoginView.vue') },
-  {
-    path: '/signup',
-    name: 'signup',
-    component: () => import('@/views/SignupView.vue'),
-    beforeEnter: (to, from, next) => {
-      if (to.query.social === 'true') {
-        next({ name: 'social-signup', query: to.query, replace: true })
-      } else {
-        next()
-      }
-    }
-  },
-  {
-    path: '/signup',
-    name: 'social-signup',
-    component: () => import('@/views/SocialSignupView.vue'),
-    meta: { layout: DefaultLayout }
-  },
+  { path: '/signup', name: 'signup', component: () => import('@/views/SignupView.vue') },
+  // {
+  //   path: '/signup',
+  //   name: 'signup',
+  //   component: () => import('@/views/SignupView.vue'),
+  //   beforeEnter: (to, from, next) => {
+  //     if (to.query.social === 'true') {
+  //       next({ name: 'social-signup', query: to.query, replace: true })
+  //     } else {
+  //       next()
+  //     }
+  //   }
+  // },
+  // {
+  //   path: '/signup',
+  //   name: 'social-signup',
+  //   component: () => import('@/views/SocialSignupView.vue'),
+  //   meta: { layout: DefaultLayout }
+  // },
   { path: '/find', name: 'find', component: () => import('@/views/FindView.vue'), meta: { layout: DefaultLayout } },
   { path: '/mypage/:userId', name: 'mypage', component: () => import('@/views/MyPageView.vue'), meta: { layout: DefaultLayout } },
   { path: '/profile/:userId', name: 'profile', component: () => import('@/views/ProfileView.vue'), meta: { layout: DefaultLayout } },
@@ -46,7 +47,8 @@ export const routes = [
         component: () => import('@/components/story/StoryWrite.vue')
       }
     ]
-  }
+  },
+  { path: '/messenger', name: 'messenger', component: () => import('@/views/MessengerView.vue') }
   // {
   //   path: '/collection',
   //   name: 'collection',
